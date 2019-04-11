049702a85edd251d9bdcc9a6ee93741e0d3f32bb
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-04-11 18:27:20 -0400

    refactored all queries to raw sql and moved user routes to a seperate file

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,8 @@
-import { generateAuthToken } from "./routes/user";
-import bcrypt from "bcryptjs";
-import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import jwt from "jsonwebtoken";
-import knex from "knex";
-import pg from "pg";
 
-dotenv.config();
-
-const db = knex({
-	client: "pg",
-	connection: {
-		host: process.env.POSTGRES_HOST,
-		user: process.env.POSTGRES_USER,
-		password: process.env.POSTGRES_PASSWORD,
-		database: process.env.POSTGRES_DB
-	},
-	debug: true
-});
+import User from "./controllers/user";
 
 const app = express();
 const port = 4000;
@@ -36,98 +19,13 @@ app.get("/", (req: express.Request, res: express.Response) => {
 });
 
 // USER routes
+// Create a user
+app.post("/users/register", User.registerUser);
 
-// login route
-app.post("/signin", (req: express.Request, res: express.Response) => {
-	let { email, password } = req.body;
-	db.select("email", "hash")
-		.from("login")
-		.where("email", "=", email)
-		.then(
-			(data): any => {
-				const isValid = bcrypt.compare(password, data[0].hash);
-				if (isValid) {
-					return db
-						.transaction((trx) => {
-							trx
-								.select("*")
-								.from("users")
-								.where("email", "=", email)
-								.then((user) => {
-									const token = generateAuthToken(email);
-									return trx("users")
-										.returning("*")
-										.whereNull("token")
-										.insert({
-											token: token
-										})
-										.then((user) => {
-											res.json(user[0]);
-										})
-										.then(trx.commit)
-										.catch(trx.rollback);
-								});
-						})
-
-						.catch((err) =>
-							res.status(400).json("Unable to retrieve user info")
-						);
-				} else {
-					res.status(400).json("Unable to retrieve user info");
-				}
-			}
-		)
-		.catch((err: any) => res.status(400).json("Invalid Credentials"));
-});
-
-// register route
-app.post("/register", async (req: express.Request, res: express.Response) => {
-	const { email, name, password } = req.body;
-
-	const hash = await bcrypt.hash(password, 10);
-	db.transaction((trx) => {
-		trx
-			.insert({
-				hash: hash,
-				email: email
-			})
-			.into("login")
-			.returning("email")
-			.then((loginEmail) => {
-				const token = generateAuthToken(email);
-
-				return trx("users")
-					.returning("*")
-					.insert({
-						email: loginEmail[0],
-						name: name,
-						token: token,
-						joined: new Date()
-					})
-					.then((user) => {
-						res.json(user[0]);
-					})
-					.then(trx.commit)
-					.catch(trx.rollback);
-			});
-	}).catch((err) => res.status(400).send(err));
-});
-
-// if user.token === null user is logged out
-// logout route
-app.post("/logout", (req: express.Request, res: express.Response) => {
-	const { email } = req.body;
-	db.select("token")
-		.from("users")
-		.where("email", "=", email)
-		.returning("*")
-		.update({token: null})
-		.then((data) => {
-			console.log(data[0])
-			res.status(200).send("logged out!");
-		})
-		.catch((err) => res.status(400).send(err));
-});
+// Sign in and generate a auth token
+app.post('/users/signin', User.signInUser)
+// sign out user
+app.post('/users/signout', User.signOutUser);
 
 app.listen(port, () => {
 	console.log(`server started at http://localhost:${port}`);
