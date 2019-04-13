e4472ea02b810c1ba9c04f30ab1465043661f815
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-04-13 12:31:13 -0400

    added rate limiting for api endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import User from "./controllers/user";
 
 const app = express();
 const port = 4000;
+
+app.enable("trust proxy");
+
+const limiter = new rateLimit({
+	windowMs: 10 * 60 * 1000,
+	max: 100
+});
+
 app.use(
 	cors({
 		exposedHeaders: ["x-auth"]
 	})
 );
-
 app.use(bodyParser.json());
+app.use('limiter');
+
+// API ROUTES
 
 app.get("/", (req: express.Request, res: express.Response) => {
 	res.status(200).send("success!");
@@ -23,9 +34,9 @@ app.get("/", (req: express.Request, res: express.Response) => {
 app.post("/users/register", User.registerUser);
 
 // Sign in and generate a auth token
-app.post('/users/signin', User.signInUser)
+app.post("/users/signin", User.signInUser);
 // sign out user
-app.post('/users/signout', User.signOutUser);
+app.post("/users/signout", User.signOutUser);
 
 app.listen(port, () => {
 	console.log(`server started at http://localhost:${port}`);
